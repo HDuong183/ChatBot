e2d076bd237806a1cb84183baffbd80a39ac1104
Hai Duong <88724391+HDuong1803@users.noreply.github.com>
2021-11-08 21:08:57 +0700

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,24 @@
+#include<bits/stdc++.h>
+#include<Windows.h>
+#include<time.h>
+#include<thread>
+#include<chrono>
+#include "dohoa.h"
+//#include "Cunghoangdao.h"
+//#include "Snakegame.h"
+#include "botngu.h"
+//#include "demsongaysong.h"
+//#include "totinh.h"
+//#include "rain.h"
+//#include "saobang.h"
+
+
+using namespace std;
+using std::this_thread::sleep_for;
+using std::chrono::milliseconds;
+
+
+int main(int argc, char** argv) {
+	runbot();
+	return 0;
+}
